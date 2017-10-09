0efee60107a19db66df0d420550b052060fe01d2
Andrii Nezhyd <andriinezhyd@gmail.com>
2017-10-09 21:27:02 +0300

    add project

---
+++ dist/js/common.js
@@ -0,0 +1,240 @@
+
+
+// some scrips for table
+var headertext = [],
+headers = document.querySelectorAll("#islands th"),
+tablerows = document.querySelectorAll("#islands th"),
+tablebody = document.querySelector("#islands tbody");
+
+for(var i = 0; i < headers.length; i++) {
+  var current = headers[i];
+  headertext.push(current.textContent.replace(/\r?\n|\r/,""));
+} 
+for (var i = 0, row; row = tablebody.rows[i]; i++) {
+  for (var j = 0, col; col = row.cells[j]; j++) {
+    col.setAttribute("data-th", headertext[j]);
+  } 
+}
+// end table code
+
+
+// beginning of the sliders code
+
+(function() {
+	
+	function Slideshow( element ) {
+		this.el = document.querySelector( element );
+		this.init();
+	}
+	
+	Slideshow.prototype = {
+		init: function() {
+			this.wrapper = this.el.querySelector( ".slider-wrapper" );
+			this.slides = this.el.querySelectorAll( ".slide" );
+			this.previous = this.el.querySelector( ".slider-previous" );
+			this.next = this.el.querySelector( ".slider-next" );
+			this.index = 0;
+			this.total = this.slides.length;
+			this.timer = null;
+			
+			this.action();
+			this.stopStart();	
+		},
+
+		_slideTo: function( slide ) {
+			var currentSlide = this.slides[slide];
+			currentSlide.style.opacity = 1;
+			
+			for( var i = 0; i < this.slides.length; i++ ) {
+				var slide = this.slides[i];
+				if( slide !== currentSlide ) {
+					slide.style.opacity = 0;
+				}
+			}
+		},
+		action: function() {
+			var self = this;
+			self.timer = setInterval(function() {
+				self.index++;
+				if( self.index == self.slides.length ) {
+					self.index = 0;
+				}
+				self._slideTo( self.index );
+			}, 5000);
+		},
+		stopStart: function() {
+			var self = this;
+			self.el.addEventListener( "mouseover", function() {
+				clearInterval( self.timer );
+				self.timer = null;
+				
+			}, false);
+			self.el.addEventListener( "mouseout", function() {
+				self.action();
+				
+			}, false);
+		}
+		
+		
+	};	
+	document.addEventListener( "DOMContentLoaded", function() {	
+		var slider = new Slideshow( "#main-slider" );		
+	});	
+})();
+// end slider
+
+
+
+// the modal windows
+
+var modal = document.getElementById("form");
+var btn = document.getElementById("getContacts");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function(){
+	modal.style.display = "flex";
+}
+span.onclick = function(){
+	modal.style.display = "none";
+}
+window.onclick = function(event){
+	if(event.target == modal){
+		modal.style.display = "none";
+	}
+}
+
+
+var modalSearch = document.getElementById("search");
+var btnSearch = document.getElementById("getSearch");
+var spanSearch = document.getElementsByClassName("closeSearch")[0];
+
+btnSearch.onclick = function(){
+	modalSearch.style.display = "block";
+}
+spanSearch.onclick = function(){
+	modalSearch.style.display = "none";
+}
+window.onclick = function(event){
+	if(event.target == modalSearch){
+		modalSearch.style.display = "none";
+	}
+}
+// end the modal windows
+
+// Simple Mobile Navigation 
+;(function($) {
+	function MobileNav(options) {
+		this.options = $.extend({
+			container: null,
+			hideOnClickOutside: false,
+			menuActiveClass: 'nav-active',
+			menuOpener: '.nav-opener',
+			menuDrop: '.nav-drop',
+			toggleEvent: 'click',
+			outsideClickEvent: 'click touchstart pointerdown MSPointerDown'
+		}, options);
+		this.initStructure();
+		this.attachEvents();
+	}
+	MobileNav.prototype = {
+		initStructure: function() {
+			this.page = $('html');
+			this.container = $(this.options.container);
+			this.opener = this.container.find(this.options.menuOpener);
+			this.drop = this.container.find(this.options.menuDrop);
+		},
+		attachEvents: function() {
+			var self = this;
+
+			if(activateResizeHandler) {
+				activateResizeHandler();
+				activateResizeHandler = null;
+			}
+
+			this.outsideClickHandler = function(e) {
+				if(self.isOpened()) {
+					var target = $(e.target);
+					if(!target.closest(self.opener).length && !target.closest(self.drop).length) {
+						self.hide();
+					}
+				}
+			};
+
+			this.openerClickHandler = function(e) {
+				e.preventDefault();
+				self.toggle();
+			};
+
+			this.opener.on(this.options.toggleEvent, this.openerClickHandler);
+		},
+		isOpened: function() {
+			return this.container.hasClass(this.options.menuActiveClass);
+		},
+		show: function() {
+			this.container.addClass(this.options.menuActiveClass);
+			if(this.options.hideOnClickOutside) {
+				this.page.on(this.options.outsideClickEvent, this.outsideClickHandler);
+			}
+		},
+		hide: function() {
+			this.container.removeClass(this.options.menuActiveClass);
+			if(this.options.hideOnClickOutside) {
+				this.page.off(this.options.outsideClickEvent, this.outsideClickHandler);
+			}
+		},
+		toggle: function() {
+			if(this.isOpened()) {
+				this.hide();
+			} else {
+				this.show();
+			}
+		},
+		destroy: function() {
+			this.container.removeClass(this.options.menuActiveClass);
+			this.opener.off(this.options.toggleEvent, this.clickHandler);
+			this.page.off(this.options.outsideClickEvent, this.outsideClickHandler);
+		}
+	};
+
+	var activateResizeHandler = function() {
+		var win = $(window),
+			doc = $('html'),
+			resizeClass = 'resize-active',
+			flag, timer;
+		var removeClassHandler = function() {
+			flag = false;
+			doc.removeClass(resizeClass);
+		};
+		var resizeHandler = function() {
+			if(!flag) {
+				flag = true;
+				doc.addClass(resizeClass);
+			}
+			clearTimeout(timer);
+			timer = setTimeout(removeClassHandler, 500);
+		};
+		win.on('resize orientationchange', resizeHandler);
+	};
+
+	$.fn.mobileNav = function(options) {
+		return this.each(function() {
+			var params = $.extend({}, options, {container: this}),
+				instance = new MobileNav(params);
+			$.data(this, 'MobileNav', instance);
+		});
+	};
+}(jQuery));
+// page init
+jQuery(function(){	
+	initMobileNav();
+});
+// mobile menu init
+function initMobileNav() {
+	jQuery('body').mobileNav({
+		hideOnClickOutside: true,
+		menuActiveClass: 'nav-active',
+		menuOpener: '.nav-opener',
+		menuDrop: '.nav-drop'
+	});
+}
+
